4edb1541bb68c832f944d30f463a2776087dba9e
nicholas keyes <bubbzthegreat@gmail.com>
2024-08-14 17:02:27 -0500

    Added new files for logo and for certificates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 function Footer() {
   return( 
   <div className="footer">
-    <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+    <div className="cert--container">
+      <img src="/public/images/Advanced-QB-Online.png" alt="" />
+      <img src="/public/images/Gold Tier Badge.png" alt="" />
+      <img src="/public/images/QB-Payroll.png" alt="" />
+    </div>
+    <p>&copy; 2024 Keyes and Associates. All rights reserved.</p>
         <div class="social-icons">
-            <a href="https://facebook.com" target="_blank">Facebook</a>
-            <a href="https://twitter.com" target="_blank">Twitter</a>
-            <a href="https://instagram.com" target="_blank">Instagram</a>
+            <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61564204352290" target="_blank">Facebook</a>
+            <a href="https://www.linkedin.com/in/keyes-and-associates-4b4738322?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7d3B0Oa%2FTdeJhfNdkH2X%2Bw%3D%3D" target="_blank">Linkedin</a>
   </div>
   <p><a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-service">Terms of Service</a></p>
   </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,9 @@ function NavBar() {
   return (
     <>
       <div className="navBar">
-        {/* <img className="logo" src="/public/images/Bookkeeping-02.jpg" alt="" /> */}
+        <div className="logo--container">
+        {/* <img className="logo" src="/public/images/Keyes-_-Assoc.-Bookkeeping-03.svg" alt="" /> */}
+        </div>
         <div className="link--container">
           <Link className="headerLink" to={"/"}>
             Home
@@ -20,14 +22,14 @@ function NavBar() {
             Contact
           </Link>
         </div>
-      <div className="icon--container">
-        <a href="">
+      {/* <div className="icon--container">
+        <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61564204352290">
         <FaLinkedinIn className="socialIcon" />
         </a>
-        <a href="">
+        <a href="https://www.linkedin.com/in/keyes-and-associates-4b4738322?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7d3B0Oa%2FTdeJhfNdkH2X%2Bw%3D%3D">
         <FaFacebookF className="socialIcon" />
         </a>
-      </div>
+      </div> */}
       </div>
     </>
   );
