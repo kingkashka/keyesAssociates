da2127632761dfc3feb6311928cbc329440d2fd1
nicholas keyes <bubbzthegreat@gmail.com>
2024-08-15 23:30:42 -0500

    updated navbar and adjusting logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,11 @@ function NavBar() {
     <>
       <div className="navBar">
         <div className="logo--container">
-        {/* <img className="logo" src="/public/images/Keyes-_-Assoc.-Bookkeeping-03.svg" alt="" /> */}
+          <img
+            className="logo"
+            src="/images/Keyes-_-Assoc.-Bookkeeping-03.svg"
+            alt=""
+          />
         </div>
         <div className="link--container">
           <Link className="headerLink" to={"/"}>
@@ -22,14 +26,14 @@ function NavBar() {
             Contact
           </Link>
         </div>
-      {/* <div className="icon--container">
-        <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61564204352290">
-        <FaLinkedinIn className="socialIcon" />
-        </a>
-        <a href="https://www.linkedin.com/in/keyes-and-associates-4b4738322?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7d3B0Oa%2FTdeJhfNdkH2X%2Bw%3D%3D">
-        <FaFacebookF className="socialIcon" />
-        </a>
-      </div> */}
+          {/* <div className="icon--container">
+            <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61564204352290">
+              <FaLinkedinIn className="socialIcon" />
+            </a>
+            <a href="https://www.linkedin.com/in/keyes-and-associates-4b4738322?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7d3B0Oa%2FTdeJhfNdkH2X%2Bw%3D%3D">
+              <FaFacebookF className="socialIcon" />
+            </a>
+          </div> */}
       </div>
     </>
   );
