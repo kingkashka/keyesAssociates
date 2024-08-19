967ac6c26c10b7a418da32dc812e447ff2c1220d
nicholas keyes <bubbzthegreat@gmail.com>
2024-08-19 16:41:29 -0500

    Added poweder blue border to navBar and underneath each section.

---
+++ src/components/LogoContainer.jsx
@@ -0,0 +1,12 @@
+function LogoContainer(){
+    return(
+        <section className="logo--container">
+          <img
+            className="logo"
+            src="/images/logo.svg"
+            alt=""
+          />
+        </section>
+    )
+}
+export default LogoContainer;
