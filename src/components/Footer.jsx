3295c42bf3345ece5b0a89c11b4293980459bf48
nicholas keyes <bubbzthegreat@gmail.com>
2024-08-25 14:33:41 -0500

    changed images back to png files and moved directory as well

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <section className="footer w-auto h-auto">
       <div className="cert--container w-auto h-auto">
-        <img src="/src/images/Advanced-QB-Online.png" alt="" />
-        <img src="/src/images/Gold-Tier-Badge.png" alt="" />
-        <img src="/src/images/QB-Payroll.png" alt="" />
+        <img src="/images/Advanced-QB-Online.png" alt="Advanced QB Online" />
+        <img src="/images/Gold-Tier-Badge.png" alt="Gold Tier Badge" />
+        <img src="/images/QB-Payroll.png" alt="QB Payroll" />
       </div>
       <p>&copy; {currentYear} Keyes and Associates. All rights reserved.</p>
       {/* <p><a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-service">Terms of Service</a></p> */}
