77823e6a6bffb41b1281f816ce774a3a21014155
nicholas keyes <bubbzthegreat@gmail.com>
2024-08-25 14:15:33 -0500

    removed alt image files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <section className="footer w-auto h-auto">
       <div className="cert--container w-auto h-auto">
-        <img src="/images/QBOnline.svg" alt="Certified Advanced QuickBooks" />
-        <img src="/images/GoldTierBadge.svg" alt="ProAdvisor Gold" />
-        <img src="/images/QBPayroll.svg" alt="Certified QuickBooks Payroll" />
+        <img src="/images/QBOnline.svg" alt="" />
+        <img src="/images/GoldTierBadge.svg" alt="" />
+        <img src="/images/QBPayroll.svg" alt="" />
       </div>
       <p>&copy; {currentYear} Keyes and Associates. All rights reserved.</p>
       {/* <p><a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-service">Terms of Service</a></p> */}
